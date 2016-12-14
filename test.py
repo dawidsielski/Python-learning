fdbc86be17b62fb77eded06b84e6fd6a45ade9bb
turbodivx <dawid.sielski@outlook.com>
2016-12-14 22:14:31 +0100

    First commit

---
+++ test.py
@@ -0,0 +1,23 @@
+import math
+import time
+
+list = [1,2,3,4,5,6]
+it = iter(list)
+
+print(math.pi)
+
+string = 'dafciooo'
+string = string.capitalize()
+print(string)
+
+for x in [1,2,3] : 
+	print (x,end=' ')
+print('\n')	
+
+dict = {'Name': 'Dawid', 'Age': 21}
+print(dict['Age'])
+
+a = len(dict)
+print(dict.keys())
+
+print (time.localtime())
---
+++ test1.py
@@ -0,0 +1,83 @@
+#def hej(imie):
+#    print('Hej ' + imie + '!')
+
+#dziewczyny = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Ty']
+#for imie in dziewczyny:
+#    hej(imie)
+#    print('Kolejna dziewczyna')
+
+
+#number = 0
+#for i in range(0,5):
+#	print("%.2f"%i)
+#	number = number +0.2
+
+
+#number = 0
+#for i in range(0,2):
+#	print("%.1f" % number)
+#	number = number +0.2
+
+
+##print numbers from 0 to 9 included
+#for i in range(0,10):
+#	print(i)
+##end
+
+
+##line should continue with "\"
+#a = 1
+#b = 2
+#c = 3
+#
+#sum = 	a + \
+#		b + \
+#		c
+#print(sum)
+##end
+
+#a = int("22", 8) #converts 22 from decimal to octal numbering system
+#print(a)
+
+# x = 10
+# while x != 0:
+# 	print("The value of x is",x,"dafcioo")
+# 	x-=1
+
+# #iterate through list
+# list = [1,2,3,4,5,6,7,8,9,10,11]
+# iterate_list = iter(list)
+# for i in iterate_list:
+# 	print(i)
+#end
+
+# import random
+# for i in range(10):
+# 	print(random.random() * 10**10	)
+
+# import math
+# print(math.cos(math.pi))
+# print(math.degrees(3)) #converts radians to degrees
+
+
+
+# tuple1, tuple2 = (1253,125) , (123,123)
+# print(tuple1)
+# print(tuple2)
+# print (min(tuple1))
+
+# dict = {"name" : 'Dawid', "surename" : "Sie", "age" : 21}
+# print(dict.keys())
+
+import time
+ticks = time.time()
+print(ticks)
+print(time.localtime())
+print(time.asctime(time.localtime()))
+clock = time.clock()
+print(clock)
+
+
+# import calendar
+# cal = calendar.month(2016,12)
+# print(cal)
