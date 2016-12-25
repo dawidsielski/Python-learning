0dc00e1ceac2d0118af01878f8ed5d949567e523
turbodivx <dawid.sielski@outlook.com>
2016-12-25 22:00:23 +0100

    ex5 practicepython.org

---
+++ practicepython.org/ex5.py
@@ -0,0 +1,15 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+common_elements = []
+
+for i in a:
+    for j in b:
+        if i == j and i not in common_elements:
+            common_elements.append(i)
+            break
+
+print(common_elements)
+
+
+# print(a.intersection(b))
