d6570299132fb27d2543a97be8b409efc9c2f421
turbodivx <dawid.sielski@outlook.com>
2016-12-26 18:21:20 +0100

    ex24 added

---
+++ practicepython.org/ex24.py
@@ -0,0 +1,18 @@
+def print_board():
+    for i in range(3):
+        for i in range(3):
+            print("---", end = " ")
+        print()
+        for i in range(6):
+            print("|", end=" ")
+        print()
+    for i in range(3):
+        print("---", end = " ")
+    print()
+
+
+
+
+
+
+print_board()
