af7cec8b77e404fa02880e1b90210f0491739c8d
turbodivx <dawid.sielski@outlook.com>
2017-02-26 17:18:42 +0100

    Guess three times (for loop and range function)

--- python101.readthedocs.io/1.Basics/1.2.1 template.py
+++ python101.readthedocs.io/1.Basics/1.2.1 template.py
@@ -1,14 +1,17 @@
 #!/ust/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Guess number having three chances"""
+
 import random
 
 number = random.randint(1, 10)
-print("Random number: ", number)
+# print("Random number: ", number)
+for i in range(3):
+    answer = input("What number from 1 to 10  do i have in my mind? ")
 
-answer = input("What number from 1 to 10  do i have in my mind?")
-
-if number == int(answer):
-    print("Congratulations! You guessed the right number!")
-else:
-    print("Try one more time.")
+    if number == int(answer):
+        print("Congratulations! You guessed the right number!")
+        break
+    else:
+        print("Try one more time.")
