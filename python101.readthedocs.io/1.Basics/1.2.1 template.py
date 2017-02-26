8344d76c6c7343b7f836421885ed52beef8b58df
turbodivx <dawid.sielski@outlook.com>
2017-02-26 17:36:47 +0100

    Elif

--- python101.readthedocs.io/1.Basics/1.2.1 template.py
+++ python101.readthedocs.io/1.Basics/1.2.1 template.py
@@ -8,10 +8,12 @@ import random
 number = random.randint(1, 10)
 # print("Random number: ", number)
 for i in range(3):
+    print("Chance number:", i + 1)
     answer = input("What number from 1 to 10  do i have in my mind? ")
-
     if number == int(answer):
         print("Congratulations! You guessed the right number!")
         break
+    elif i == 2:
+        print("My number was:", number)
     else:
         print("Try one more time.")
