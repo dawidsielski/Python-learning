001b28a97121b09429506d145ca9707ed188e141
turbodivx <dawid.sielski@outlook.com>
2017-02-26 17:11:17 +0100

    Guessing the number

--- python101.readthedocs.io/1.Basics/1.2.1 template.py
+++ python101.readthedocs.io/1.Basics/1.2.1 template.py
@@ -4,4 +4,11 @@
 import random
 
 number = random.randint(1, 10)
-print(number)
+print("Random number: ", number)
+
+answer = input("What number from 1 to 10  do i have in my mind?")
+
+if number == int(answer):
+    print("Congratulations! You guessed the right number!")
+else:
+    print("Try one more time.")
