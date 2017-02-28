2468fa17d4ad00a478706b7fe44ec405fd921719
turbodivx <dawid.sielski@outlook.com>
2017-02-28 12:15:07 +0100

    Lambda functions

--- python101.readthedocs.io/1.Basics/1.6 python usefull hacks.py
+++ python101.readthedocs.io/1.Basics/1.6 python usefull hacks.py
@@ -7,7 +7,8 @@ print()
 
 country = { "country":"Poland", "capital":"Warsaw"}
 def print_countries(country, capital):
-    print("{0} is the capital of {1}".format(capital, country))
+    """Prints countries in the following format: "[capital] is the capital of [country]." """
+    print("{0} is the capital of {1}.".format(capital, country))
 
 print_countries(**country)
 
@@ -37,9 +38,19 @@ words = ['anna', 'ala', 'ela', 'wiola', 'ola']
 print([ [name, name[0]] for name in words if len(name) == 3 ])
 
 def increment(number):
+    """Increments number by 1"""
     return number + 1
 
 numbers = [1,3,5,7]
 print("Incremented numbers before: ", numbers)
 incremented_numbers = list(map(increment,numbers))
 print("Incremented numbers: ", incremented_numbers)
+
+a = 1
+print([[a for y in range(3)] for x in range(2)])
+
+print([ (x,y) for x in range(5) for y in range(3) ])
+
+
+numbers_bigger_than_four = filter(lambda x: x > 4, incremented_numbers)
+print("Numbers bigger than four ",list(numbers_bigger_than_four))
