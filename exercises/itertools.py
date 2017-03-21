66afe35058a59ff1282930a205e59ace0b3bd673
Dawid Sielski <dawid.sielski@outlook.com>
2017-03-21 13:51:22 +0100

    Itertools exercises

---
+++ exercises/itertools.py
@@ -0,0 +1,49 @@
+from itertools import *
+
+a = [0,1,2,3,4,5,6,7,8,9]
+b = ["zero", "one", "two", "three", "four", "five", "six"]
+c = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+
+
+for element in chain(a, b, c):
+    print(element)
+
+print(list(chain(a, b, c)))
+
+
+# print(count(1, 10, 1))
+
+print(list(islice(a, 2, 5 , 2)))
+print(list(islice(count(), 2, 5 , 1)))
+
+
+binary_filter = [1,0,0,0,0,0,0,1,1,0]
+print(list(compress(a, binary_filter)))
+
+
+def square(n):
+    """Returns square od a number"""
+    return n**2
+
+print(list(map(square, a)))
+
+print("Only odd numbers from a {}".format(list(filter(lambda x: x % 2, a))))
+
+print(list(islice(cycle("ABC"), 10)))
+
+#groupby
+
+persons = [ {"name": "Dawid", "surename": "Sielski"},
+            {"name": "Anna", "surename": "Sielski"},
+            {"name": "Martyna", "surename": "Arbuz"},
+            {"name": "Daria", "surename": "Rower"} ]
+
+surename = lambda x: x['surename']
+
+group_by_surename = groupby(sorted(persons, key = surename), surename)
+
+for key, group in group_by_surename:
+    print(key)
+    for value in group:
+        print(value)
+    print()
