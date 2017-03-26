c17330890d01f877550f8061bd96204510d4e340
Dawid Sielski <dawid.sielski@outlook.com>
2017-03-26 13:30:42 +0200

    Math

--- exercises/math.py
+++ exercises/math.py
@@ -3,3 +3,16 @@ print(math.ceil(2.3))
 print(math.copysign(3,-0.0))
 print(math.fabs(-3.55))
 print(math.factorial(5))
+print(math.floor(3.55))
+print(math.frexp(2))
+
+print(sum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1]))
+print(math.fsum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1]))
+
+print("GDC: {}".format(math.gcd(12,15)))
+
+print(math.exp(1))
+
+print(math.log(4,2))
+print(math.pow(4,2))
+print(math.sqrt(4))
