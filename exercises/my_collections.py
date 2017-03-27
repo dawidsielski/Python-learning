25a3d4f6297ee678f2cee241b0d02fd79505048d
Dawid Sielski <dawid.sielski@outlook.com>
2017-03-28 00:05:34 +0200

    Deque

--- exercises/my_collections.py
+++ exercises/my_collections.py
@@ -12,3 +12,41 @@ print(sorted(my_counter.elements()))
 print(my_counter.most_common(1))
 
 
+#deque
+print()
+
+d = collections.deque('dawidd')
+print(d)
+
+for character in d:
+    print(character.upper(), end = " ")
+
+d.append("aa")
+d.appendleft("x")
+print(d)
+
+print(d.pop())
+print(d)
+
+print(d.popleft())
+print(d)
+
+print(list(d))
+print(set(d))
+
+print()
+
+print(d)
+print(collections.deque(reversed(d)))
+
+print()
+
+print(d)
+d.extend('iop')
+print(d)
+
+print()
+
+print(d)
+d.extendleft('iop')
+print(d)
