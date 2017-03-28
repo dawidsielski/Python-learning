1a2ea9549f497cf62e094b526d7a628b8de62db0
Dawid Sielski <dawid.sielski@outlook.com>
2017-03-28 23:32:43 +0200

    Heap vol 2

--- exercises/heap.py
+++ exercises/heap.py
@@ -1,2 +1,20 @@
 import heapq
 
+l = [1,7,3,0,2,87,23,65,34,89,2]
+print(heapq.nlargest(2, l))
+print(heapq.nsmallest(2, l))
+
+h = []
+
+heapq.heappush(h, (1, "Dawid"))
+heapq.heappush(h, (2, "Ilona"))
+heapq.heappush(h, (5, "Marta"))
+heapq.heappush(h, (4, "Tomasz"))
+heapq.heappush(h, (3, "Jacek"))
+
+print(h)
+
+print(heapq.heappop(h))
+print(heapq.heappop(h))
+print(heapq.heappop(h))
+print(h)
