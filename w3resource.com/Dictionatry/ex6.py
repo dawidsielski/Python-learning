3b1a3dfc4b937290d0ca946a309e04381f4d999b
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-03 23:21:22 +0200

    Dictionary vol 1

---
+++ w3resource.com/Dictionatry/ex6.py
@@ -0,0 +1,7 @@
+d = {}
+
+n = int(input("Enter number: "))
+for i in range(1,n + 1):
+    d[i] = i**2
+
+print(d)
---
+++ w3resource.com/Dictionatry/ex7.py
@@ -0,0 +1,6 @@
+d = dict()
+
+for element in range(1,16):
+    d[element] = element**2
+
+print(d)
