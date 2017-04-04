c053709971e585073695cba38d4a648b90c16421
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-04 23:56:22 +0200

    Numpy array training

---
+++ numpy_lib/array.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+a = np.array([1,2,3])
+print(a)
+print(type(a))
+x = a.shape
+print(x)
+print(x[0])
+
+
+b = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(b.shape)
+print(b.shape[0])
+print(b.shape[1])
+print(b[0,1])
+
+a = np.zeros((2,2))
+print(a)
+a = np.ones((2,2))
+print(a)
+a = np.eye(2)
+print(a)
+
+a = np.random.random((4,4)) * 10
+print(a)
