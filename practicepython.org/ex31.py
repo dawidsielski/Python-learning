0a7734108a836a948cb554d9ce45fc3991852e33
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-13 00:08:29 +0200

    Print message to user

--- practicepython.org/ex31.py
+++ practicepython.org/ex31.py
@@ -8,14 +8,18 @@ for element in guessing_word_list:
 guesses_available = 6
 wrong_guesses = 0
 correct_guesses = 0
+user_guesses = []
 while correct_guesses < len(guessing_word) and wrong_guesses < 6:
     user_guess = str(input("\nEnter user guess: ")).upper()
+    while user_guess in user_guesses:
+        user_guess = str(input("You have already tried entering \"" + user_guess.upper() + "\" letter. Try another one: ")).upper()
+    user_guesses.append(user_guess)
     if user_guess[0] not in guessing_word_list:
         wrong_guesses += 1
         print("Wrong guess! Guesses left: {}".format(guesses_available - wrong_guesses))
     else:
         for element in table:
-            if element[0] == user_guess[0] and  element[1] == False:
+            if element[0] == user_guess[0] and element[1] == False:
                 correct_guesses += 1
                 element[1]= True
 
@@ -26,6 +30,6 @@ while correct_guesses < len(guessing_word) and wrong_guesses < 6:
                 print("_", end = "")
 
 if correct_guesses == len(guessing_word):
-    print("\nEnd game!")
+    print("\nYou win!")
 else:
     print("\nYou lose!")
