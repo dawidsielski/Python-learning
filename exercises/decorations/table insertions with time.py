6a8a94f957ac85987c555927a385df85aa8e2237
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-19 23:53:29 +0200

    decorations done

--- exercises/decorations/table insertions with time.py
+++ exercises/decorations/table insertions with time.py
@@ -1,9 +1,22 @@
+import time
+
+def time_decorate(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print("Execution of " + func.__name__ + " took " + str((end - start) * 1000) + " in miliseconds.")
+        return result
+    return wrapper
+
+@time_decorate
 def square_list(l):
     result = []
     for element in l:
         result.append(element ** 2)
     return result
 
+@time_decorate
 def cube_list(l):
     result = []
     for element in l:
@@ -11,5 +24,5 @@ def cube_list(l):
     return result
 
 
-print(square_list(range(10)))
-print(cube_list(range(10)))
+square_list(range(1000000))
+cube_list(range(1000000))
