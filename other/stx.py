187c2c3dd6eb2e986797aa22d500923996b1a304
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-21 00:46:25 +0200

    Stx exercise improve

--- other/stx.py
+++ other/stx.py
@@ -2,3 +2,6 @@ from collections import Counter
 l = [1,2,3,4,4,3,2,3,6,4,7,4,99,99,99]
 c = Counter(l)
 print(c)
+
+user_number = int(input("Enter number: "))
+print("Your number appears " + str(c[user_number]) + " times in " + str(l))
