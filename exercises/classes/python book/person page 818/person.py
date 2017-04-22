fc63e6eb40083d73268efeb4afe364d4e8c4838c
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-22 21:24:53 +0200

    Class Person simple constructor

---
+++ exercises/classes/python book/person page 818/person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name , job, pay):
+        self.name = name
+        self. job = job
+        self.pay = pay
+
+    
+def main():
+    person1 = Person("Dawid", "Sielski", 22)
+
+
+if __name__ == "__main__":
+    main()
