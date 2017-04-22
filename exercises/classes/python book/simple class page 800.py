a180ff2cea8f9656b9cd896fd23426b325ad638f
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-22 20:07:07 +0200

    Simple class and inheritance

---
+++ exercises/classes/python book/simple class page 800.py
@@ -0,0 +1,25 @@
+class Anything:
+    def set_data(self, data):
+        self.data = data
+    
+    def get_data(self):
+        return self.data
+
+class Anything2(Anything):
+    def display(self):
+        print(self.data)
+
+
+
+def main():
+    x = Anything()
+    x.set_data("Dawid")
+    print(x.get_data())
+
+    y = Anything2()
+    y.set_data("Kazimierz")
+    y.display()
+
+
+if __name__ == "__main__":
+    main()
