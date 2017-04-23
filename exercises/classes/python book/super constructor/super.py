b9d50ccf54bb53e64bc8a5cf1519a82e96171cb3
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-23 18:24:56 +0200

    Calling super improvments

--- exercises/classes/python book/super constructor/super.py
+++ exercises/classes/python book/super constructor/super.py
@@ -12,8 +12,15 @@ class Sub2(Super):
         super().__init__(x)
         self.y = y
 
-x = Super(1)
-y = Sub(2,3)
-z = Sub2(4,5)
 
-print(z.x)
+
+def main():
+    x = Super(1)
+    y = Sub(2,3)
+    z = Sub2(4,5)
+
+    print(z.x) #to check super().__init__ calling
+
+
+if __name__ == "__main__":
+    main()
