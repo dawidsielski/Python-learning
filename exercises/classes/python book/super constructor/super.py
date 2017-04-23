4967e778c6fb99dac0d8ce55a3c10f0b5a49a6bf
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-23 18:23:15 +0200

    Calling super()

---
+++ exercises/classes/python book/super constructor/super.py
@@ -0,0 +1,19 @@
+class Super:
+    def __init__(self, x):
+        self.x = x
+
+class Sub(Super):
+    def __init__(self, x, y):
+        Super.__init__(self, x)
+        self.y = y
+
+class Sub2(Super):
+    def __init__(self, x, y):
+        super().__init__(x)
+        self.y = y
+
+x = Super(1)
+y = Sub(2,3)
+z = Sub2(4,5)
+
+print(z.x)
