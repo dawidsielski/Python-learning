a420afb8c71c8307edf99ba3c205dad6cb5a37a2
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-24 01:36:45 +0200

    ex1 datetime

--- w3resource.com/Date Time/ex1.py
+++ w3resource.com/Date Time/ex1.py
@@ -1,1 +1,10 @@
 import datetime
+
+print(datetime.datetime.now())
+print(datetime.datetime.today().year)
+print(datetime.datetime.today().month)
+print(datetime.datetime.today().day)
+print(datetime.datetime.today().hour)
+print(datetime.datetime.today().minute)
+print(datetime.datetime.today().second)
+print(datetime.datetime.today().microsecond)
