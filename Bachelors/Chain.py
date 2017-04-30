4dea6e6118d1cbc7aac1934716a4c0333bdd0a0d
Dawid Sielski <dawid.sielski@outlook.com>
2017-04-30 21:24:45 +0200

    FIrst and last pixel

--- Bachelors/Chain.py
+++ Bachelors/Chain.py
@@ -3,11 +3,25 @@ from PIL import Image, ImageMorph
 tree = Image.open("Acer campestre 1.png")
 # tree.show()
 
-pixels = tree.load()
 print(tree.size[0])
 print(tree.size[1])
 
-print(pixels[1000,1000])
-# tree.show()
 
-# print(dir(tree))
+def first_black_pixel(tree):
+    pixels = tree.load()
+    for height in range(tree.size[1]):
+        for width in range(tree.size[0]):
+            if pixels[width, height] == 1:
+                return (width, height)
+
+def last_black_pixel(tree):
+    pixels = tree.load()
+    for height in reversed(range(tree.size[1])):
+        for width in reversed(range(tree.size[0])):
+            if pixels[width, height] == 1:
+                return (width, height)
+
+    
+
+print(first_black_pixel(tree))
+print(last_black_pixel(tree))
