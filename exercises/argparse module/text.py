314a49cad64bd9d45b42214f945ee6ce23b28832
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-02 23:51:56 +0200

    Choices added to argparse

--- exercises/argparse module/text.py
+++ exercises/argparse module/text.py
@@ -5,7 +5,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("number1", help="First number")
     parser.add_argument("number2", help="Second number")
-    parser.add_argument("operation", help="operation")
+    parser.add_argument("operation", help="operation", choices = ["add", "multiply"])
     parser.add_argument("--print_message", help="What to print after") #optional argument | arguments orded can be mixed
     args = parser.parse_args()
 
@@ -16,6 +16,8 @@ def main():
 
     if args.operation == "add":
         print(int(args.number1) + int(args.number2))
+    if args.operation == "multiply":
+        print(int(args.number1) * int(args.number2))
 
     print(args.print_message)
 
