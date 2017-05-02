aafbe64ad96ce979699d704d7493d13a25df8a18
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-02 23:48:55 +0200

    Argparse optional arguments

--- exercises/argparse module/text.py
+++ exercises/argparse module/text.py
@@ -6,6 +6,7 @@ def main():
     parser.add_argument("number1", help="First number")
     parser.add_argument("number2", help="Second number")
     parser.add_argument("operation", help="operation")
+    parser.add_argument("--print_message", help="What to print after") #optional argument | arguments orded can be mixed
     args = parser.parse_args()
 
     print(args.number1)
@@ -15,6 +16,8 @@ def main():
 
     if args.operation == "add":
         print(int(args.number1) + int(args.number2))
+
+    print(args.print_message)
 
 if __name__ == "__main__":
     main()
