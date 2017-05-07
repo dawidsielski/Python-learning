737860976759bdab2c1c7bcfb1e66efb8449d5df
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-07 20:25:11 +0200

    Prepare update.

--- Bachelors/prepare images/prepare.py
+++ Bachelors/prepare images/prepare.py
@@ -7,16 +7,11 @@ print(os.getcwd())
 path = r'C:\Users\Dawid\Documents\Studies\Programowanie\Python-learning\Bachelors'
 trees2_absolute_path = os.path.join(path, 'trees2')
 
-trees2 = open("trees2_paths.txt", "w")
-
 
 for root, dirname, filenames in os.walk(trees2_absolute_path):
     print(root)
     print("Dirname: ", dirname)
     for name in filenames:
         # print(name)
-        # print(os.path.join(root, name))
-        trees2.write(os.path.join(root, name))
-        trees2.write("\n")
-
-trees2.close()
+        image_path = os.path.join(root, name)
+        img = Image.open(image_path)
