c1a873c512e66bc2e807c4fce3155c4bd8c483d8
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-13 22:12:44 +0200

    Check.py update

--- exercises/check.py
+++ exercises/check.py
@@ -1,2 +1,18 @@
 x = sum([ x for x in [1,2,3,11,22] if (x >= 10 and x <= 99) or (x >= -99 and x <= -10)])
 print(x)
+
+def solution(X):
+    result = [0 for x in range(X)]
+    for i in range(1,X + 1):
+        for dwarf_number in range(1, X + 1):
+            x = i * dwarf_number
+            if x > X:
+                break
+            if result[x - 1] == 0:
+                result[x - 1] = 1
+            else:
+                result[x - 1] = 0
+        print(result)
+    return result
+
+print('Solution: ' + str(solution(3)))
