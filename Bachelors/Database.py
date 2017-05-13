67cf297eb685349bcbe4e4a5a60c76e0372a95b5
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-13 18:49:25 +0200

    Database.py update to new CHain constructor

--- Bachelors/Database.py
+++ Bachelors/Database.py
@@ -13,16 +13,14 @@ absolute_path_trees = os.path.join(script_dir, "trees")
 trees_content = os.listdir(absolute_path_trees)
 # print(len(trees_content))
 
-trees_chain_code = shelve.open('trees_database')
+trees_chain_code = shelve.open('trees_database check')
 
 for element in trees_content:
     if element in trees_chain_code:
         continue
     print(element)
     path = os.path.join(absolute_path_trees, element)
-    tree = Image.open(path)
-    imfile = tree.convert("1", dither = Image.NONE)
-    c = Chain(imfile)
+    c = Chain(path)
     information = {'Code' : c.chain_code, "Border pixel" : c.points, "Perimeter" : c.perimeter, \
                     "Shape height" : c.shape_height, "Shape width" : c.shape_width, "First black" : c.begin, "Last black" : c.end}
     trees_chain_code[element] = information
