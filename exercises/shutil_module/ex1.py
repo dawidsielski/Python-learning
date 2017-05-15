9c95a1fd99534e8431804ec629bd7d25b3729175
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-15 15:53:46 +0200

    Shutil copyfile

---
+++ exercises/shutil_module/ex1.py
@@ -0,0 +1,9 @@
+import shutil
+import os
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+print(script_path)
+
+# shutil.copy(str(os.path.join(script_path, "folder1", "something.txt")), str(os.path.join(script_path, "folder2")))
+
+shutil.copytree(str(os.path.join(script_path, "folder1")), str(os.path.join(script_path, "folder3")))
