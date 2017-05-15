916cb0114353a833ed7edafe34e50a6972c2d87a
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-15 23:08:45 +0200

    Pytest ex2

--- exercises/pytest module/dwarf/test_dwarf.py
+++ exercises/pytest module/dwarf/test_dwarf.py
@@ -4,3 +4,14 @@ import pytest
 def test_dwarf():
     result = dwarf.solution(3)
     assert result == [1, 0, 0]
+
+@pytest.mark.parametrize('input, expected',
+                        [
+                            (1, [1]),
+                            (2, [1,0]),
+                            (3, [1,0,0])
+                        ]
+                        )
+def test_dwarf_2(input, expected):
+    result = dwarf.solution(input)
+    assert result == expected
