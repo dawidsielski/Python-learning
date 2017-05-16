5bf400dd721af91e20e7ade093cbf0ace3da2e84
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-16 11:37:15 +0200

    Skip tests pytest module

--- exercises/pytest module/skip/test_operations.py
+++ exercises/pytest module/skip/test_operations.py
@@ -1,20 +1,17 @@
 import operations
 import pytest
+import sys
 
 def test_multiplication():
     result = operations.multiplication(3, 6)
     assert result == 18
 
+@pytest.mark.skipif(sys.version_info > (3,0), reason = "No reason.")
 def test_subtraction():
     result = operations.subtraction(3, 6)
     assert result == -3
 
-@pytest.mark.parametrize("test_input, expected_output",
-                         [   
-                            (2, 4),
-                            (3, 10)
-                         ]
-                        )
-def test_power(test_input, expected_output):
-    result = operations.power(test_input)
-    assert result == expected_output
+@pytest.mark.skip(reason="Not working.")
+def test_power():
+    result = operations.power(6)
+    assert result == 36
