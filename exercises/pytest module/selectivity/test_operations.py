ded066e4c281323adc0fa7f5aea3388dd82bdb2b
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-16 11:26:14 +0200

    Selectivity pytest

--- exercises/pytest module/selectivity/test_operations.py
+++ exercises/pytest module/selectivity/test_operations.py
@@ -1,20 +1,22 @@
 import operations
 import pytest
 
+@pytest.mark.linuks
 def test_multiplication():
     result = operations.multiplication(3, 6)
     assert result == 18
 
+@pytest.mark.mac
 def test_subtraction():
     result = operations.subtraction(3, 6)
     assert result == -3
 
-@pytest.mark.parametrize("test_input, expected_output",
-                         [   
-                            (2, 4),
-                            (3, 10)
-                         ]
-                        )
-def test_power(test_input, expected_output):
-    result = operations.power(test_input)
-    assert result == expected_output
+@pytest.mark.windows
+def test_power():
+    result = operations.power(5)
+    assert result == 25
+
+
+# -k [name] do select specified test (name can be a part of function name)
+# -m [category name] for selecting category for the tests
+# can write [not [category name]] to run every test but specified in command line
