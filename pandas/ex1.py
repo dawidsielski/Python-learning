592c7fe6cea25710e39aff08ab731df6c9fa100a
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-19 00:09:05 +0200

    Pandas learning

--- pandas/ex1.py
+++ pandas/ex1.py
@@ -3,10 +3,14 @@ import os
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 
-csv_file = open(os.path.join(script_path, "Cities.csv"))
+with open(os.path.join(script_path, "Cities.csv")) as csv_file:
+    read = pd.read_csv(csv_file)
 
-read = pd.read_csv(csv_file)
+
 print(read)
+print("print the beginning:\n", read.head())
+print("print the tail:\n", read.tail())
 
+print(read.sort_values(by=['country','temperature'],ascending=[True,False]))
 
-csv_file.close()
+print(read.city)
