403da8011f0a656c60681f35fe609991f8a3f6a0
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-18 23:31:51 +0200

    Pandas initial

--- pandas/ex1.py
+++ pandas/ex1.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import os
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+csv_file = open(os.path.join(script_path, "Cities.csv"))
+
+read = pd.read_csv(csv_file)
+print(read)
+
+
+csv_file.close()
