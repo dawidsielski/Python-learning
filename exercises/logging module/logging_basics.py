ea61ebe9b76128c7f1aec8f91eac9ab98da4a73f
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-21 15:31:30 +0200

    Logging basics

---
+++ exercises/logging module/logging_basics.py
@@ -0,0 +1,26 @@
+"""
+Learning how to log
+"""
+import logging
+
+logging.basicConfig(filename="logging_basics.log", level=logging.INFO,
+                    format="%(asctime)s:%(funcName)s:%(message)s")
+
+def add(a_value, b_value):
+    """Function takes two argument a and b and returns the sum a + b"""
+    logging.info("Function run with argments %i and %i", a_value, b_value)
+    return a_value + b_value
+
+def multiply(a_value, b_value):
+    """Function takes two argument a and b and returns the multiplication a * b"""
+    logging.info("Function run with argments %i and %i", a_value, b_value)
+    return a_value * b_value
+
+def main():
+    """Function prepared for testing purposes"""
+    for a_value, b_value in enumerate(range(10)):
+        add(a_value, b_value)
+        multiply(a_value, b_value)
+
+if __name__ == "__main__":
+    main()
