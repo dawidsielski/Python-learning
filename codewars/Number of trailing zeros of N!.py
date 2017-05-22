14c1aa23b5dfc2970da7b2a63acd2bd0fed60ea4
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-22 21:57:24 +0200

    Can do without checking 2

--- codewars/Number of trailing zeros of N!.py
+++ codewars/Number of trailing zeros of N!.py
@@ -9,17 +9,39 @@ zeros(12) = 2 # 1 * 2 * 3 .. 12 = 479001600
 that has 2 trailing zeros 4790016(00)
 Be careful 1000! has length of 2568 digital numbers.
 """
+
+
+def Trailing_Zeroes(x):
+    fives = 0
+    for number in range(2, x+1):          
+        while number > 0:
+            if number % 5 == 0:
+                fives += 1
+                number = number/5
+            else:
+                break
+    return fives
+
+
 def zeros(n):
-    result = 1
-    for number in range(1,n + 1):
-        result *= number
-    count = 0
-    for element in str(result)[::-1]:
-        if element == "0":
-            count += 1
-        else:
-            break
-    return count
+    count_2, count_5 = 0, 0
+    for element in range(1, n + 1):
+        # if element % 2 == 0:
+        #     number = element
+        #     while number % 2 == 0 and number != 0:
+        #         number = number / 2
+        #         count_2 += 1
+        if element % 5 == 0:
+            number = element
+            while number % 5 == 0 and number != 0:
+                number = number / 5
+                count_5 += 1
+    return min(count_2,count_5)
 
 
 print(zeros(12) == 2)
+print(zeros(13) == 2)
+print(zeros(20) == 4)
+print(zeros(25) == 6)
+print(zeros(30) == 7)
+print(zeros(1000) == 249)
