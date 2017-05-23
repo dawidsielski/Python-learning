6ea77f8a15794d1880d8547c3878dd73990de81a
Dawid Sielski <dawid.sielski@outlook.com>
2017-05-23 12:50:37 +0200

    Look and say needs update

---
+++ codewars/lock and say and sum.py
@@ -0,0 +1,60 @@
+"""
+Get the digits sum of nth number from the Look-and-Say sequence(1-based).
+
+1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211, ...
+
+Input/Output
+
+[input] integer n
+
+nth number in the sequence to get where 1 <= n <= 55 and n=1 is "1".
+
+[output] an integer
+
+The sum of digits in nth number from the Look-and-Say sequence.
+
+Example
+
+For n = 2, the output shoule be 2.
+
+"11" --> 1 + 1 --> 2
+
+For n = 3, the output shoule be 3.
+
+"21" --> 2 + 1 --> 3
+
+For n = 4, the output shoule be 5.
+
+"1211" --> 1 + 2 + 1 + 1 --> 5
+"""
+
+def look_and_say_and_sum(n):
+    starting_number = [1]
+    result = [1]
+    d = {}
+
+    for element in range(n - 1):
+        for digit in starting_number:
+            if digit in d.keys():
+                d[digit] += 1
+            else:
+                d[digit] = 1
+        # print(d)
+        result.clear()
+        for key, value in d.items():
+            result.append(value)
+            result.append(key)
+        starting_number = result
+        d.clear()
+    print(starting_number)
+    return sum(result)
+            
+        
+        
+
+
+print(look_and_say_and_sum(1) == 1)
+print(look_and_say_and_sum(2) == 2)
+print(look_and_say_and_sum(3) == 3)
+print(look_and_say_and_sum(4) == 5)
+print(look_and_say_and_sum(5) == 8)
