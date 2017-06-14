1ae0deb3879b50fec17421954a20ca9a65f5e0be
Dawid Sielski <dawid.sielski@outlook.com>
2017-06-14 23:40:27 +0200

    Url improve

--- modules training/url/wheather_information.py
+++ modules training/url/wheather_information.py
@@ -1,9 +1,9 @@
-import urllib.request
 import json
 import pprint
+import urllib.request
 
 city = input("Please enter city: ")
-wheather_api_url = 'http://samples.openweathermap.org/data/2.5/weather?q={city}&appid=c8381001d25d70edbdadb3d6444f44e5'.format(city = city)
+wheather_api_url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=c8381001d25d70edbdadb3d6444f44e5'.format(city = city)
 
 with urllib.request.urlopen(wheather_api_url) as wheather_json_file:
     wheather_information = json.loads(wheather_json_file.read().decode())
