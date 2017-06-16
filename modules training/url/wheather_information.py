2cfdc1b0c470e36233c922e7ad0fa0ba26f1676d
Dawid Sielski <dawid.sielski@outlook.com>
2017-06-16 14:54:02 +0200

    Added argparse

--- modules training/url/wheather_information.py
+++ modules training/url/wheather_information.py
@@ -1,13 +1,33 @@
+import argparse
 import json
 import pprint
 import urllib.request
 
-city = input("Please enter city: ")
-wheather_api_url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=c8381001d25d70edbdadb3d6444f44e5'.format(city = city)
 
-with urllib.request.urlopen(wheather_api_url) as wheather_json_file:
-    wheather_information = json.loads(wheather_json_file.read().decode())
+def wheather_data(city):
+    wheather_api_url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=c8381001d25d70edbdadb3d6444f44e5'.format(city = city)
 
-# pprint.pprint(wheather_information)
+    with urllib.request.urlopen(wheather_api_url) as wheather_json_file:
+        wheather_information = json.loads(wheather_json_file.read().decode())
 
-print("Temperature in {city} is {temp}".format(temp = wheather_information['main']['temp'], city = city))
+    return wheather_information
+
+
+def main():
+    # pprint.pprint(wheather_information)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--city", help = "City from which temperature info will be obtain.")
+
+    args = parser.parse_args()
+
+    # city = input("Please enter city: ")
+
+    city = args.city
+
+    wheather_information = wheather_data(city)
+
+    print("Temperature in {city} is {temp}".format(temp = wheather_information['main']['temp'], city = city))
+
+if __name__ == "__main__":
+    main()
