1b19c35718d6f5b8197957f4a7c6e36f0e676338
Dawid Sielski <dawid.sielski@outlook.com>
2017-06-17 15:23:26 +0200

    Added verbose

--- modules training/url/wheather_information.py
+++ modules training/url/wheather_information.py
@@ -15,22 +15,28 @@ def wheather_data(city):
 
 
 def main():
-    # pprint.pprint(wheather_information)
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--city", help = "City from which temperature info will be obtain.")
     parser.add_argument("-i", "--input",action = 'store_true', help = "Input from keyboard during execution.")
 
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-v', '--verbose', action = 'store_true', help = "Print all data.")
+
     args = parser.parse_args()
 
     if args.input:
         city = input("Please enter city: ")
+    else:
+        city = args.city
 
-    city = args.city
 
     wheather_information = wheather_data(city)
 
-    print("Temperature in {city} is {temp}".format(temp = wheather_information['main']['temp'], city = city))
+    if args.verbose:
+        pprint.pprint(wheather_information)
+    else:
+        print("Temperature in {city} is {temp}".format(temp = wheather_information['main']['temp'], city = city))
 
 if __name__ == "__main__":
     main()
