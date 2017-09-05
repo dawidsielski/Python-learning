0ed032eb0fb270c8c20e3eddd255d86927e1c860
Dawid Sielski <dawid.sielski@outlook.com>
2017-09-05 22:03:08 +0200

    PIL learning

--- modules training/python_image_library/img.py
+++ modules training/python_image_library/img.py
@@ -1,3 +1,16 @@
 from PIL import Image
 im = Image.open("lena.png")
 im.rotate(45).show()
+
+
+import glob, os
+
+size = 128, 128
+
+
+print(glob.glob("*.*"))
+for infile in glob.glob("*.jpg"):
+    file, ext = os.path.splitext(infile)
+    im = Image.open(infile)
+    im.thumbnail(size)
+    im.save(file + ".thumbnail", "JPEG")
